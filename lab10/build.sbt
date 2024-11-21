f9b4682e6948b2194351fc99e9251f9ff0f427c6
Navin Mohan <navinmohan81@gmail.com>
2024-11-21 12:24:06 -0500

    Add lab10

---
+++ lab10/build.sbt
@@ -0,0 +1,10 @@
+name := """time-ws"""
+organization := "com.example"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.15"
+
+libraryDependencies += guice
---
+++ lab10/test/controllers/HomeControllerTest.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.WithApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.route;
+
+public class HomeControllerTest extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder().build();
+    }
+
+    @Test
+    public void testIndex() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+
+        List<String> l = new ArrayList<>();
+        l.add("a");
+
+        String s = "a a a";
+        String k = "b";
+
+        long count = l.stream().filter(s::contains).count();
+        System.out.println(count);
+    }
+
+}
